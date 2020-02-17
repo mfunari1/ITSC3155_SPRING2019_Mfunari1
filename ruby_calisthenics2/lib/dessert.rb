0c2af9ff7908087f0dbf018c8de41dac64d2047a
mfunari1 <mfunari1@uncc.edu>
2020-02-17 21:15:15 +0000

    Ruby OOP - Part 1

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -2,18 +2,36 @@ class Dessert
   # add code for setters and getters
   def initialize(name, calories)
     # your code here
+   
+    
+     @name = name
+    @calories = calories
+
+   
+    
   end
+  
+  attr_accessor :name
+  attr_accessor :calories
   def healthy?
     # your code here
+    @calories < 200
   end
   def delicious?
     # your code here
+     true
   end
 end
 
 class JellyBean < Dessert
   # add code for setters and getters
+  attr_accessor :flavor
   def initialize(flavor)
     # your code here
+     super(name, calories)
+    @name = name
+    @calories = calories
+    @flavor = flavor
+    
   end
 end
